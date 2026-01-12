5e51b7b2c36b4a4d95fd1961777f81544e926649
Nibir <nibirdeka70@gmail.com>
2026-01-12 23:38:41 +0530

    PWA added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,15 @@ import "./index.css";
 import AuthProvider from "./context/AuthContext.jsx";
 import { AdminProvider } from "./context/AdminContext.jsx";
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(() => console.log('Service Worker registered'))
+      .catch(err => console.error('Service Worker registration failed', err))
+  })
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <AuthProvider>
       <AdminProvider>
